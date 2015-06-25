a47520401a380a9f828249115a08aac87c0ce72b
Janis Miezitis <janjiss@gmail.com>
2015-06-26 01:20:24 +0300

    Bumped the version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ElixirStream.Mixfile do
 
   def project do
     [app: :elixir_stream,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
