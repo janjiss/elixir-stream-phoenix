b2a211abdb719bc85aa41dd66e0d05b67e614562
DainisL <dainis186@gmail.com>
2015-06-28 21:53:38 +0300

    fixed error

--- web/controllers/feed_controller.ex
+++ web/controllers/feed_controller.ex
@@ -1,9 +1,7 @@
 defmodule ElixirStream.FeedController do
   use ElixirStream.Web, :controller
   alias ElixirStream.Entry
-
   plug :action
-  plug :accepts, ["xml"]
 
   def index(conn, _params) do
     entries = Repo.all from e in Entry, order_by: [desc: e.id], preload: [:user]
--- web/controllers/sitemap_controller.ex
+++ web/controllers/sitemap_controller.ex
@@ -1,12 +1,12 @@
 defmodule ElixirStream.SitemapController do
   use ElixirStream.Web, :controller
   alias ElixirStream.Entry
-
   plug :action
-  plug :accepts, ["xml"]
 
   def index(conn, _params) do
     entries = Repo.all from e in Entry, order_by: [desc: e.id], preload: [:user]
-    conn |> render "index.xml", entries: entries
+    conn
+    |> put_resp_content_type("application/xml")
+    |> render "index.xml", entries: entries
   end
 end
