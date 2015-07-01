6eb8a5cc2bbd3000377b207afb2311d908a9a207
Janis Miezitis <janjiss@gmail.com>
2015-07-01 15:44:56 +0300

    Revert to old version

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule ElixirStream.Mixfile do
       {:phoenix_live_reload, "~> 0.4", only: :dev},
       {:cowboy, "~> 1.0"},
       {:comeonin, "~> 0.8"},
-      {:exrm, "~> 0.17.3"},
+      {:exrm, github: "janjiss/exrm"},
       {:earmark, "~> 0.1.17"},
       {:timex, "~> 0.15.0"}
     ]
