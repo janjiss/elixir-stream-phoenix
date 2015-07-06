5e9c66295fe6f6aaa9944715507554910b3764e6
DainisL <dainis186@gmail.com>
2015-07-06 20:46:12 +0300

    added user select to admin form

--- web/views/admin/entry_view.ex
+++ web/views/admin/entry_view.ex
@@ -1,6 +1,14 @@
 defmodule ElixirStream.Admin.EntryView do
   use ElixirStream.Web, :view
   alias ElixirStream.Entry
+  alias ElixirStream.User
+  alias ElixirStream.Repo
+
+
+  def all_users do
+    import Ecto.Query, only: [from: 2]
+    Repo.all(from(u in User, select: {u.username, u.id} ) )
+  end
 
   def author_name(%Entry{author_name: author_name}) when author_name != nil do
     author_name
