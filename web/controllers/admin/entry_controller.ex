63389bd2c22fd190ba26bb484f342d860a453633
DainisL <dainis186@gmail.com>
2015-07-06 21:26:54 +0300

    added entry create for admin

--- web/controllers/admin/entry_controller.ex
+++ web/controllers/admin/entry_controller.ex
@@ -15,4 +15,31 @@ defmodule ElixirStream.Admin.EntryController do
     |> put_layout("admin.html")
     |> render "new.html", changeset: changeset
   end
+
+  def create(conn, %{"entry" => entry_params}) do
+    changeset = Entry.changeset_with_admin(%Entry{}, entry_params)
+    if changeset.valid? do
+      entry = ElixirStream.Repo.insert!(changeset)
+      conn
+      |> put_flash(:info, "Entry created successfully.")
+      |> redirect(to: admin_entry_path(conn, :index))
+    else
+      conn
+      |> put_layout("admin.html")
+      |> render "new.html", changeset: changeset
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    entry = Repo.one(from(e in Entry, where: e.id == ^id ))
+    if entry do
+      Repo.delete(entry)
+      message = "Entry deleted successfully."
+    else
+      message = "Not found"
+    end
+    conn
+    |> put_flash(:info, message)
+    |> redirect(to: admin_entry_path(conn, :index))
+  end
 end
